2b547b983cc2afb96e0615b89ed98f4194d9e0ac
Priyanshu.sde <priyanshu.sde@gmail.com>
2025-07-10 03:06:59 +0530

    feat: configure CORS with specific origin, credentials, and headers, add response id to deploy endpoint, and initialize logsMap for each id before logging

--- Upload_Service/src/index.ts
+++ Upload_Service/src/index.ts
@@ -16,7 +16,14 @@ publisher.connect();
 subscriber.connect();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://skydeploy.priyanshu.online', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.post("/deploy", async (req, res) => {
@@ -38,7 +45,7 @@ app.post("/deploy", async (req, res) => {
   await publisher.hSet("status",id,"Uploaded");
 
   res.json({
-   
+    id: id
   });
 });
 
@@ -46,14 +53,14 @@ app.get("/status",async (req,res) => {
   const id = req.query.id;
   const response = await subscriber.hGet("status",id as string);
   res.json({
-  
+    status: response
   })
 })
 
 const logsMap : Record<string,string[]> = {};
 
 function logForId(id : string, line: string){
- 
+  if(!logsMap[id]) logsMap[id] = [];
   logsMap[id].push(line);
 }
 
