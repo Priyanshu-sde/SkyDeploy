669507d153cbdc8bf1a2b9b5564524b8f9ee1c30
Priyanshu.sde <priyanshu.sde@gmail.com>
2025-07-11 04:09:51 +0530

    refactor: update ecosystem.config.js and package.json files to use compiled JavaScript instead of TypeScript for Deploy-service, Upload_Service, and request-handler

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,47 +3,44 @@ module.exports = {
     {
       name: 'deploy-service',
       cwd: './Deploy-service',
-      script: 'ts-node',
-      args: 'src/index.ts',
+      script: 'node',
+      args: 'dist/index.js',
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '200M',
+      max_memory_restart: '150M',
       env_file: '../.env',
       log_file: '/var/log/pm2/deploy-service.log',
       out_file: '/var/log/pm2/deploy-service-out.log',
-      error_file: '/var/log/pm2/deploy-service-error.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+      error_file: '/var/log/pm2/deploy-service-error.log'
     },
     {
       name: 'request-handler',
       cwd: './request-handler', 
-      script: 'ts-node',
-      args: 'src/index.ts',
+      script: 'node',
+      args: 'dist/index.js',
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '200M',
+      max_memory_restart: '150M',
       env_file: '../.env',
       log_file: '/var/log/pm2/request-handler.log',
       out_file: '/var/log/pm2/request-handler-out.log',
-      error_file: '/var/log/pm2/request-handler-error.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+      error_file: '/var/log/pm2/request-handler-error.log'
     },
     {
       name: 'upload-service',
       cwd: './Upload_Service',
-      script: 'ts-node',
-      args: 'src/index.ts', 
+      script: 'node',
+      args: 'dist/index.js', 
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '200M',
+      max_memory_restart: '150M',
       env_file: '../.env',
       log_file: '/var/log/pm2/upload-service.log',
       out_file: '/var/log/pm2/upload-service-out.log',
-      error_file: '/var/log/pm2/upload-service-error.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+      error_file: '/var/log/pm2/upload-service-error.log'
     }
   ]
 };
